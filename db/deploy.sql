3a159197030ed7fe753adb59bcad131386c19636
Doug Wade <prekolna@gmail.com>
2013-07-19 21:39:54 -0500

    Added deploy script and user table

---
+++ db/deploy.sql
@@ -0,0 +1,132 @@
+﻿begin;
+create table jabp_user
+(
+	user_id serial primary key
+	,email_address text
+	,hash_pipe_salt text
+);
+commit;
+
+create table user_token
+(
+	user_id integer references jabp_user (user_id)
+	,temporary_token text
+	,token_expiry timestamp
+);
+
+create table post
+(
+	post_id serial primary key
+	,post_text text
+	,post_title text
+	,posted_by_id integer
+);
+
+create table tag
+(
+	tag_id serial primary key
+	,tag_display_name text
+);
+
+create table map_post_to_tag
+(
+	tag_id integer references tag (tag_id)
+	,post_id integer references post (post_id)
+);
+
+create function insert_user
+(
+	new_user_email text
+	,new_user_hash_pipe_salt text
+	,out new_user_id int
+)
+returns integer
+as
+$$ begin
+insert into japb_user
+(
+	email_address
+	,hash_pipe_salt
+)
+values (new_user_email, new_user_hash_pipe_salt)
+returning user_id into new_user_id;
+end $$
+language plpgsql;
+
+create function update_user_password
+(
+	user_id int
+	,updated_user_hash_pipe_salt text
+	,temporary_token text
+)
+returns boolean
+as
+$$ begin
+if exists (
+	select user_id
+	from japb_user
+	inner join user_token
+		on user_token.user_id = japb_user.user_id
+	where temporary_token = user_token.temporary_token
+		and japb_user.user_id = user_id
+		and user_token.expiry_date < current_timestamp
+)
+then
+	update japb_user
+	set hash_pipe_salt = updated_user_hash_pipe_salt;
+else
+	delete
+	from user_token
+	where user_id = user_id;
+end if;
+end $$
+language plpgsql;
+
+create function insert_post
+(
+	new_post_title text
+	,new_post_text text
+	,out new_post_id int
+)
+returns integer
+as
+$$ begin
+insert into post
+(
+	post_text
+	,post_title
+)
+values (new_post_text, new_post_title)
+returning post_id into new_post_id;
+end $$
+language plpgsql;
+
+create function delete_post
+(
+	former_post_id int
+)
+returns void
+as
+$$ begin
+delete from post
+where post.post_id = former_post_id;
+return;
+end $$
+language plpgsql;
+
+create function update_post
+(
+	existing_post_id int
+	,new_post_title text
+	,new_post_text text
+)
+returns void
+as
+$$ begin
+update post
+set post_text = new_post_text
+	,post_title = new_post_title
+where post.post_id = existing_post_id;
+return;
+end $$
+language plpgsql;
