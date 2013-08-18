68e844b899391eb85e46c2dba9ec2a296d4360dd
prekolna <prekolna@gmail.com>
2013-08-17 22:56:30 -0500

    Added pg deploy script

---
+++ deploy.js
@@ -0,0 +1,15 @@
+var pg = require('pg')
+  , fs = require('fs')
+  , connectionString = process.env.DATABASE_URL
+  , client
+  , query;
+
+client = new pg.Client(connectionString);
+client.connect();
+fs.readFile('./db/deploy.sql', {encoding:'utf8'}, function(err, queryString){
+    if (err){
+        console.log(err);
+    }
+    query = client.query(queryString);
+    query.on('end', function() { client.end(); });
+});
